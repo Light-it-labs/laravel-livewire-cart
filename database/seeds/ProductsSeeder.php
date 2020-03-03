6938d8509712a4d0998f73ec62fb23ec18f4670a
Juan Vargas <juan@lightit.io>
2020-03-03 13:38:00 -0300

    Add product data seeder

---
+++ database/seeds/ProductsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Product;
+use Illuminate\Database\Seeder;
+
+class ProductsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Product::class)->create(50);
+    }
+}
