292e1623bb5087985dad0e19ea275e90682745c5
Juan Vargas <juan@lightit.io>
2020-03-03 13:36:04 -0300

    Add product table and factory

--- database/migrations/2020_03_03_134741_create_products_table.php
+++ database/migrations/2020_03_03_134741_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+            $table->string('description');
+            $table->float('price');
+
             $table->timestamps();
         });
     }
