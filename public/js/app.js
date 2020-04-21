d0c4a12ba9b55a15420bfc0aac2652036df41b96
Juan Vargas <juanmvg2006@gmail.com>
2020-04-21 10:16:23 -0300

    Fix pagination error

--- public/js/app.js
+++ public/js/app.js
@@ -19359,8 +19359,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/juan/projects/laravel-livewire-how-to/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/juan/projects/laravel-livewire-how-to/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/juan/Projects/laravel-livewire-cart/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/juan/Projects/laravel-livewire-cart/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/layouts/pagination.blade.php
+++ resources/views/layouts/pagination.blade.php
@@ -1,15 +1,51 @@
-@if ($paginator->lastPage() > 1)
-    <ul >
-        <li class="{{ ($paginator->currentPage() == 1) ? ' disabled' : '' }} inline border-t border-b border-l border-brand-light px-3 py-2 no-underline">
-            <a href="{{ $paginator->url(1) }}"><<</a>
-        </li>
-        @for ($i = 1; $i <= $paginator->lastPage(); $i++)
-            <li class="{{ ($paginator->currentPage() == $i) ? ' text-green-500' : '' }} inline border-t border-b border-l border-brand-light px-3 py-2 no-underline">
-                <a href="{{ $paginator->url($i) }}">{{ $i }}</a>
+@if ($paginator->hasPages())
+    <ul class="pagination" role="navigation">
+        @if ($paginator->onFirstPage())
+            <li class="inline border-0 border-brand-light px-3 py-2 no-underline disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
+                <span class="page-link" aria-hidden="true">
+                    <span class="d-none d-md-block">&lsaquo;</span>
+                    <span class="d-block d-md-none">@lang('pagination.previous')</span>
+                </span>
             </li>
-        @endfor
-        <li class="{{ ($paginator->currentPage() == $paginator->lastPage()) ? ' disabled' : '' }} inline border-t border-b border-r border-l border-brand-light px-3 py-2 no-underline">
-            <a href="{{ $paginator->url($paginator->currentPage()+1) }}" >>></a>
-        </li>
+        @else
+            <li class="inline border-0 border-brand-light px-3 py-2 no-underline">
+                <button type="button" class="page-link" wire:click="previousPage" rel="prev" aria-label="@lang('pagination.previous')">
+                    <span class="d-none d-md-block">&lsaquo;</span>
+                    <span class="d-block d-md-none">@lang('pagination.previous')</span>
+                </button>
+            </li>
+        @endif
+
+        @foreach ($elements as $element)
+            @if (is_string($element))
+                <li class="inline border-0 border-brand-light px-3 py-2 no-underline disabled d-none d-md-block" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+            @endif
+
+            @if (is_array($element))
+                @foreach ($element as $page => $url)
+                    @if ($page == $paginator->currentPage())
+                        <li class="text-green-500 inline border-0 border-brand-light px-3 py-2 no-underline active d-none d-md-block" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                    @else
+                        <li class="inline border-0 border-brand-light px-3 py-2 no-underline d-none d-md-block"><button type="button" class="page-link" wire:click="gotoPage({{ $page }})">{{ $page }}</button></li>
+                    @endif
+                @endforeach
+            @endif
+        @endforeach
+
+        @if ($paginator->hasMorePages())
+            <li class="inline border-0 border-brand-light px-3 py-2 no-underline">
+                <button type="button" class="page-link" wire:click="nextPage" rel="next" aria-label="@lang('pagination.next')">
+                    <span class="d-block d-md-none">@lang('pagination.next')</span>
+                    <span class="d-none d-md-block">&rsaquo;</span>
+                </button>
+            </li>
+        @else
+            <li class="inline border-0 border-brand-light px-3 py-2 no-underline disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
+                <span class="" aria-hidden="true">
+                    <span class="d-block d-md-none">@lang('pagination.next')</span>
+                    <span class="d-none d-md-block">&rsaquo;</span>
+                </span>
+            </li>
+        @endif
     </ul>
 @endif
